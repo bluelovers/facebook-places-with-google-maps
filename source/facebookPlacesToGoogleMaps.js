f474d90dc7efeda216f7681a6951fabfb1060b43
gnanaswaroop@gmail.com <gnanaswaroop@gmail.com@9e502b3c-065a-69a5-b169-763bfdf7195f>
2010-10-30 18:24:22 +0000

    Removing the older file
    git-svn-id: http://facebook-places-with-google-maps.googlecode.com/svn/trunk@4 9e502b3c-065a-69a5-b169-763bfdf7195f

--- source/facebookPlacesToGoogleMaps.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name           Facebook Places - With Google Maps
-// @namespace      http://swaroop.in
-// @description    Facebook places by default has Bing maps, but if you wish to have Google Maps instead. This is the right script for you
-// @include        http://www.facebook.com/#!/pages/*
-// @require        http://ajax.microsoft.com/ajax/jquery/jquery-1.4.2.min.js
-// ==/UserScript==
-
-alert($('iframe[class=map_iframe]'));
